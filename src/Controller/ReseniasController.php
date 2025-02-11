17d0baf2aa8c2b032d1a43dcb9678bff78db4b1b
Alberto <sanabriaalbertog@gmail.com>
2025-02-11 19:03:00 +0100

    resenias/all funciona en el backen poniendo: http://127.0.0.1:8000/resenias/all

--- src/Controller/ReseniasController.php
+++ src/Controller/ReseniasController.php
@@ -1,36 +1,68 @@
 <?php
 
+
 namespace App\Controller;
 
+use App\Repository\ReseniasRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/resenias')]
+#[Route('/resenias', name: 'app_resenias')]
 final class ReseniasController extends AbstractController
 {
-    #[Route('/all', name: 'app_resenias', methods: ['GET'])]
+    #[Route('/all', name: 'app_resenias_all', methods: ['GET'])]
     public function getAllResenias(ReseniasRepository $reseniasRepository): Response
     {
-        $listaResenias = array_map(function ($resenia) {
+        return $this->json($this->mapResenias($reseniasRepository->findAllResenias()));
+    }
+
+    private function mapResenias(array $resenias): array
+    {
+        return array_map(function ($resenia) {
             return [
-                'id' => $resenia->getId(),
-                'id_producto' => $resenia->getIdProducto(),
-                'id_usuario' => $resenia->getIdUsuario(),
-                'calificacion' => $resenia->getCalificacion(),
-                'comentario' => $resenia->getComentario(),
+                'id' => $resenia['id'],
+                'id_producto' => $resenia['id_producto'],
+                'id_usuario' => $resenia['id_usuario'],
+                'comentario' => $resenia['comentario'],
+                'calificacion' => $resenia['calificacion'],
             ];
-        }, $reseniasRepository->findAll());
-        return $this->json($listaResenias);
+        }, $resenias);
     }
 }
+
+//
+//namespace App\Controller;
+//
+//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+//use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\Routing\Attribute\Route;
+//
+//#[Route('/resenias')]
 //final class ReseniasController extends AbstractController
 //{
-//    #[Route('/resenias', name: 'app_resenias')]
-//    public function index(): Response
+//    #[Route('/all', name: 'app_resenias', methods: ['GET'])]
+//    public function getAllResenias(ReseniasRepository $reseniasRepository): Response
 //    {
-//        return $this->render('resenias/index.html.twig', [
-//            'controller_name' => 'ReseniasController',
-//        ]);
+//        $listaResenias = array_map(function ($resenia) {
+//            return [
+//                'id' => $resenia->getId(),
+//                'id_producto' => $resenia->getIdProducto(),
+//                'id_usuario' => $resenia->getIdUsuario(),
+//                'calificacion' => $resenia->getCalificacion(),
+//                'comentario' => $resenia->getComentario(),
+//            ];
+//        }, $reseniasRepository->findAll());
+//        return $this->json($listaResenias);
 //    }
 //}
+////final class ReseniasController extends AbstractController
+////{
+////    #[Route('/resenias', name: 'app_resenias')]
+////    public function index(): Response
+////    {
+////        return $this->render('resenias/index.html.twig', [
+////            'controller_name' => 'ReseniasController',
+////        ]);
+////    }
+////}
--- src/Repository/ReseniasRepository.php
+++ src/Repository/ReseniasRepository.php
@@ -1,5 +1,6 @@
 <?php
 
+
 namespace App\Repository;
 
 use App\Entity\Resenias;
@@ -15,6 +16,42 @@ class ReseniasRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Resenias::class);
     }
+
+    /**
+     * @return Resenias[] Returns an array of Resenias objects
+     */
+    public function findAllResenias(): array
+    {
+        return $this->createQueryBuilder('r')
+            ->select(
+                'r.id',
+                'IDENTITY(r.id_producto) AS id_producto',
+                'IDENTITY(r.id_usuario) AS id_usuario',
+                'r.comentario',
+                'r.calificacion'
+            )
+            ->orderBy('r.id', 'ASC')
+            ->getQuery()
+            ->getResult();
+    }
+}
+
+//namespace App\Repository;
+//
+//use App\Entity\Resenias;
+//use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+//use Doctrine\Persistence\ManagerRegistry;
+//
+///**
+// * @extends ServiceEntityRepository<Resenias>
+// */
+//class ReseniasRepository extends ServiceEntityRepository
+//{
+//    public function __construct(ManagerRegistry $registry)
+//    {
+//        parent::__construct($registry, Resenias::class);
+//    }
+    // -------------------------------------------------------------------------
 
     //    /**
     //     * @return Resenias[] Returns an array of Resenias objects
@@ -40,4 +77,4 @@ class ReseniasRepository extends ServiceEntityRepository
     //            ->getOneOrNullResult()
     //        ;
     //    }
-}
+//}
