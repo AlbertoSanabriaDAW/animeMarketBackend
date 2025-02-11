c74e1bfa26aeb01d889cfee742f87e79a68000d4
Alberto <sanabriaalbertog@gmail.com>
2025-02-11 15:49:10 +0100

    añado los filtros por tematica (todavia tengo que probarlo)

--- src/Controller/ProductosController.php
+++ src/Controller/ProductosController.php
@@ -31,4 +31,97 @@ final class ProductosController extends AbstractController
         }, $productosRepository->findAll());
         return $this->json($listaProductos);
     }
+    #[Route('/bobobo', name: 'app_productos_bobobo', methods: ['GET'])]
+    public function getBoboboproductos(ProductosRepository $productosRepository): Response
+    {
+        $listaProductos = array_filter(array_map(function ($producto) {
+            return [
+                'id' => $producto->getId(),
+                'nombre' => $producto->getNombre(),
+                'imagen' => $producto->getImagen(),
+                'descripcion' => $producto->getDescripcion(),
+                'precio' => $producto->getPrecio(),
+                'id_tematica' => $producto->getIdTematica(),
+            ];
+        }, $productosRepository->findAll()), function ($producto) {
+            return $producto['id_tematica'] == 1;
+        });
+
+        return $this->json(array_values($listaProductos));
+    }
+
+    #[Route('/dragonball', name: 'app_productos_dragonball', methods: ['GET'])]
+    public function getDragonballproductos(ProductosRepository $productosRepository): Response
+    {
+        $listaProductos = array_filter(array_map(function ($producto) {
+            return [
+                'id' => $producto->getId(),
+                'nombre' => $producto->getNombre(),
+                'imagen' => $producto->getImagen(),
+                'descripcion' => $producto->getDescripcion(),
+                'precio' => $producto->getPrecio(),
+                'id_tematica' => $producto->getIdTematica(),
+            ];
+        }, $productosRepository->findAll()), function ($producto) {
+            return $producto['id_tematica'] == 2;
+        });
+
+        return $this->json(array_values($listaProductos));
+    }
+    #[Route('/doraemon', name: 'app_productos_doraemon', methods: ['GET'])]
+    public function getDoraemonproductos(ProductosRepository $productosRepository): Response
+    {
+        $listaProductos = array_filter(array_map(function ($producto) {
+            return [
+                'id' => $producto->getId(),
+                'nombre' => $producto->getNombre(),
+                'imagen' => $producto->getImagen(),
+                'descripcion' => $producto->getDescripcion(),
+                'precio' => $producto->getPrecio(),
+                'id_tematica' => $producto->getIdTematica(),
+            ];
+        }, $productosRepository->findAll()), function ($producto) {
+            return $producto['id_tematica'] == 3;
+        });
+
+        return $this->json(array_values($listaProductos));
+    }
+    #[Route('/pokemon', name: 'app_productos_pokemon', methods: ['GET'])]
+    public function getPokemonproductos(ProductosRepository $productosRepository): Response
+    {
+        $listaProductos = array_filter(array_map(function ($producto) {
+            return [
+                'id' => $producto->getId(),
+                'nombre' => $producto->getNombre(),
+                'imagen' => $producto->getImagen(),
+                'descripcion' => $producto->getDescripcion(),
+                'precio' => $producto->getPrecio(),
+                'id_tematica' => $producto->getIdTematica(),
+            ];
+        }, $productosRepository->findAll()), function ($producto) {
+            return $producto['id_tematica'] == 4;
+        });
+
+        return $this->json(array_values($listaProductos));
+    }
+
+    #[Route('/kimetsu', name: 'app_productos_kimetsu', methods: ['GET'])]
+    public function getKimetsuproductos(ProductosRepository $productosRepository): Response
+    {
+        $listaProductos = array_filter(array_map(function ($producto) {
+            return [
+                'id' => $producto->getId(),
+                'nombre' => $producto->getNombre(),
+                'imagen' => $producto->getImagen(),
+                'descripcion' => $producto->getDescripcion(),
+                'precio' => $producto->getPrecio(),
+                'id_tematica' => $producto->getIdTematica(),
+            ];
+        }, $productosRepository->findAll()), function ($producto) {
+            return $producto['id_tematica'] == 5;
+        });
+
+        return $this->json(array_values($listaProductos));
+    }
+
 }
